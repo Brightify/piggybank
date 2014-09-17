a24bf7c09d7ea10826c0c6a4de558744864a0988
Matouš Hýbl <hyblmatous@gmail.com>
2014-09-17 17:54:08 +0200

    Add abstract activity to make using Piggy Bank easier

--- example/src/main/java/org/brightify/piggybank/example/MainActivity.java
+++ example/src/main/java/org/brightify/piggybank/example/MainActivity.java
@@ -1,16 +1,15 @@
 package org.brightify.piggybank.example;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import org.brightify.piggybank.PiggyBank;
+import org.brightify.piggybank.PiggyBankActivity;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends PiggyBankActivity {
 
     private static final String SKU = "piggy_donation";
 
@@ -20,27 +19,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        PiggyBank.Builder builder = new PiggyBank.Builder(this);
-        builder.setSKU(SKU);
-        builder.showPrice(true);
-        piggyBank = builder.build();
-
-        piggyBank.onCreate();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        piggyBank.onDestroy();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        piggyBank.onActivityResult(requestCode, resultCode, data);
+    protected PiggyBank getPiggyBank() {
+        if (piggyBank == null) {
+            PiggyBank.Builder builder = new PiggyBank.Builder(this);
+            builder.setSKU(SKU);
+            //builder.showPrice(true);
+            piggyBank = builder.build();
+        }
+        return piggyBank;
     }
 
     @Override
@@ -52,7 +41,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.donate) {
-            piggyBank.donate(new PiggyBank.DonateCallback() {
+            getPiggyBank().donate(new PiggyBank.DonateCallback() {
                 @Override
                 public void onSuccess() {
                     Toast.makeText(MainActivity.this, "Donated.", Toast.LENGTH_SHORT).show();
@@ -60,7 +49,7 @@ public class MainActivity extends Activity {
 
                 @Override
                 public void onFailure(String reason) {
-                    Toast.makeText(MainActivity.this, "Something went wrong while donating. "+ reason,
+                    Toast.makeText(MainActivity.this, "Something went wrong while donating. " + reason,
                             Toast.LENGTH_SHORT).show();
                 }
             });
---
+++ library/src/main/java/org/brightify/piggybank/PiggyBankActivity.java
@@ -0,0 +1,32 @@
+package org.brightify.piggybank;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @author <a href="mailto:hyblmatous@gmail.com">Matous Hybl</a>
+ */
+public abstract class PiggyBankActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPiggyBank().onCreate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getPiggyBank().onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        getPiggyBank().onActivityResult(requestCode, resultCode, data);
+    }
+
+    protected abstract PiggyBank getPiggyBank();
+}
